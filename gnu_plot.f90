98a1b35ff43b92bdbcda20c007377f54b5a5bc44
Simon Geard <simon@whiteowl.co.uk>
2017-04-09 20:22:26 +0100

    Enhancements to line plots

--- gnu_plot.f90
+++ gnu_plot.f90
@@ -8,7 +8,7 @@ module gnu_plot
     integer, parameter :: plot_type_lines  = 2
 
     type, abstract :: gplot_t
-        character(len=:), allocatable :: fstem
+        character(len=:), allocatable :: gfile
         character(len=:), allocatable :: title
         character(len=:), allocatable :: xlabel
         character(len=:), allocatable :: ylabel
@@ -28,10 +28,13 @@ module gnu_plot
     end type scatter
 
     type, extends(gplot_t) :: line
-        integer :: plot_type = plot_type_lines
-        logical :: plot_is_square = .false.
+        integer                       :: plot_type = plot_type_lines
+        logical                       :: plot_is_square = .false.
+        character(len=:), allocatable :: legend
+        character(len=:), allocatable :: pos
     contains
         procedure :: write => write_gpl_line
+        procedure :: append => append_gpl_line
     end type line
 
 contains
@@ -49,14 +52,14 @@ contains
 
     subroutine append_plot(fname, plot_command)
         character(len=*), intent(in) :: fname, plot_command
-        character(len=64)  :: cmsg
-        character(len=120) :: line
-        integer            :: cstat
-        integer            :: u
+        character(len=64)   :: cmsg
+        character(len=1000) :: line
+        integer             :: cstat
+        integer             :: u
         
         open(newunit=u,file=fname//'gpl',status='old',position='append',action='readwrite')
         backspace(u)
-        read(u,'(a120)') line
+        read(u,'(a1000)') line
         backspace(u)
         write(u,fmt='(a)',advance='no') trim(line)//', '//plot_command
         close(u)
@@ -73,34 +76,35 @@ contains
         character(len=*), intent(in)   :: data_file
         real(8), optional,  intent(in) :: ymax
         integer           :: u
-        character(len=:), allocatable :: fgpl
-        fgpl = this%fstem//'gpl'
-        open (newunit=u,file=fgpl,access='sequential',status='replace')
+        character(len=:), allocatable  :: fstem
+        
+        open (newunit=u,file=this%gfile,access='sequential',status='replace')
         write (u,'(a)') 'set xlabel "'//this%xlabel//'"'
         write (u,'(a)') 'set ylabel "Frequency"'
         write (u,'(a)') 'set title "'//this%title//'"'
         write (u,'(a)') 'set boxwidth 1 relative'
         write (u,'(a)') 'set term png truecolor'
-        write (u,'(a)') 'set output "'//this%fstem//'png'
+        fstem = this%gfile(1:len(this%gfile)-3)
+        write (u,'(a)') 'set output "'//fstem//'png'
         write (u,'(a)') 'set style fill transparent solid 0.5 noborder'
         write (u,'(a)') 'set xtics rotate 90'
         if (present(ymax)) then
             write(u,'(a,f0.2,a)') 'set yrange [0:',ymax,']'
-        end if    
+        end if
         write (u,'(a)') 'set datafile separator ","'
         write (u,fmt='(a)',advance='no') 'plot "'//data_file//'" using 2:3:xticlabels(1) w boxes lc rgb"red" notitle'
         close(u)
-        call create_plot(fgpl)
+        call create_plot(this%gfile)
     end subroutine write_gpl_hist
     
     ! Write gnuplot file for scatter plots
     subroutine write_gpl_scat(this,data_file)
         class(scatter), intent(in)   :: this
         character(len=*), intent(in) :: data_file
-        integer           :: u
-        character(len=:), allocatable :: fgpl
-        fgpl = this%fstem//'gpl'
-        open (newunit=u,file=fgpl,access='sequential',status='replace')
+        integer                       :: u
+        character(len=:), allocatable :: fstem
+       
+        open (newunit=u,file=this%gfile,access='sequential',status='replace')
         if (allocated(this%xlabel)) then
             write (u,'(a)') 'set xlabel "'//this%xlabel//'"'
         end if
@@ -111,10 +115,11 @@ contains
             write (u,'(a)') 'set title "'//this%title//'"'
         end if
         write (u,'(a)') 'set term png truecolor'
-        write (u,'(a)') 'set output "'//this%fstem//'png"'
+        fstem = this%gfile(1:len(this%gfile)-3)
+        write (u,'(a)') 'set output "'//fstem//'png"'
         write (u,fmt='(a)',advance='no') 'plot "'//data_file//'"'
         close(u)
-        call create_plot(fgpl)
+        call create_plot(this%gfile)
     end subroutine write_gpl_scat
     
     ! Write gnuplot file for line plots
@@ -126,20 +131,25 @@ contains
         integer                       :: u
         character(len=:), allocatable :: fgpl, ptype
         character(len=20)             :: col_sel
+        character(len=:), allocatable :: plot_command
+        character(len=:), allocatable :: fstem
         
-        fgpl = this%fstem//'gpl'
-        open (newunit=u,file=fgpl,access='sequential',status='replace')
+        open (newunit=u,file=this%gfile,access='sequential',status='replace')
         if (allocated(this%xlabel)) then
-            write (u,'(a)') 'set xlabel "'//this%xlabel//'"'
+            write (u,'(a)') 'set xlabel '//this%xlabel
         end if
         if (allocated(this%ylabel)) then
-            write (u,'(a)') 'set ylabel "'//this%ylabel//'"'
+            write (u,'(a)') 'set ylabel '//this%ylabel
         end if
         if (allocated(this%title)) then
             write (u,'(a)') 'set title "'//this%title//'"'
         end if
+        if (allocated(this%pos)) then
+            write (u,'(a)') 'set key '//this%pos
+        end if
         write (u,'(a)') 'set term png truecolor'
-        write (u,'(a)') 'set output "'//this%fstem//'png"'
+        fstem = this%gfile(1:len(this%gfile)-3)
+        write (u,'(a)') 'set output "'//fstem//'png"'
         select case(this%plot_type)
         case (plot_type_lines)
             ptype = ' with lines'
@@ -157,12 +167,69 @@ contains
             col_sel = 'using x:y'
             write(col_sel(7:7),'(i1)') columns(1)
             write(col_sel(9:9),'(i1)') columns(2)
-            write (u,fmt='(a)',advance='no') 'plot "'//data_file//'" '//trim(col_sel)//' '//ptype//' notitle'
+            plot_command = 'plot "'//data_file//'" '//trim(col_sel)//' '//ptype
         else
-            write (u,fmt='(a)',advance='no') 'plot "'//data_file//'"'//ptype//' notitle'
+            plot_command = 'plot "'//data_file//'"'//ptype
         end if
+        if (allocated(this%legend)) then
+            plot_command = plot_command//' title "'//this%legend//'"'
+        end if
+        write(u,'(a)') plot_command
         close(u)
-        call create_plot(fgpl)
+        call create_plot(this%gfile)
     end subroutine write_gpl_line
+    
+    ! Write gnuplot file for line plots
+    subroutine append_gpl_line(this,data_file,columns,force_create)
+        class(line), intent(in)       :: this
+        character(len=*), intent(in)  :: data_file
+        integer, optional, intent(in) :: columns(2)
+        logical, optional, intent(in) :: force_create
+        
+        integer                       :: u
+        character(len=1000)           :: line
+        character(len=20)             :: col_sel
+        character(len=:), allocatable :: plot_command, ptype
+        logical                       :: gfile_exists
+        logical                       :: create_new
+        
+        inquire(file=this%gfile,exist=gfile_exists)
+        create_new = (.not. gfile_exists)
+        if (.not. create_new .and. present(force_create)) then
+            create_new = force_create
+        end if
+        if (force_create) then
+            call this%write(data_file, columns)
+            return
+        end if
+        open(newunit=u,file=this%gfile,status='old',position='append',action='readwrite')
+        backspace(u)
+        read(u,'(a1000)') line
+        backspace(u)
+        select case(this%plot_type)
+        case (plot_type_lines)
+            ptype = ' with lines'
+        case (plot_type_points)
+            ptype = ' with points'
+        case default
+            ptype = ' with linespoints'
+        end select
+         
+        ! Apply column selector if given
+        if (present(columns)) then
+            col_sel = 'using x:y'
+            write(col_sel(7:7),'(i1)') columns(1)
+            write(col_sel(9:9),'(i1)') columns(2)
+            plot_command = '"'//data_file//'" '//trim(col_sel)//' '//ptype
+        else
+            plot_command = '"'//data_file//'"'//ptype
+        end if
+        if (allocated(this%legend)) then
+            plot_command = plot_command//' title "'//this%legend//'"'
+        end if
+        write(u,fmt='(a)',advance='no') trim(line)//', '//plot_command
+        close(u)
+
+    end subroutine append_gpl_line
 
 end module gnu_plot
